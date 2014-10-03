2bf23e58779b7651ba3c0b4d6f3478a86e6af6d6
Daniel Fernández <dfernandez@vairix.com>
2014-10-03 14:58:27 -0300

    adds sidebar in the home template and add options in site settings to fill out the sidebar

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: plugin_discourse_sidebar
+# about: create and view status of zendesk ticket within each topic
+# authors: Vairix
+register_asset "javascripts/admin/templates/site_settings/setting_text.hbs"
+register_asset "javascripts/discourse/templates/discovery.hbs"
+
+register_asset "stylesheets/sidebar_styles.css"
