d806e936468cfad93ebb002ca6b8d0865d1350c4
Arpit Jalan <arpit@techapj.com>
2016-02-03 08:00:27 +0530

    FIX: order via date created desc

--- plugin.rb
+++ plugin.rb
@@ -13,22 +13,22 @@ after_initialize do
     attributes :solved_answers
 
     def solved_answers
-      Rails.cache.fetch("latest_solved_answers", expires_in: 1.minute){
+      Rails.cache.fetch("latest_solved_answers", expires_in: 1.minute) do
         solved_answers = {}
         custom_field = TopicCustomField.includes(:topic => :category)
                                  .joins("INNER JOIN posts ON posts.id = topic_custom_fields.value::int8")
                                  .joins("INNER JOIN users ON users.id = posts.user_id")
                                  .select('topic_custom_fields.*, posts.id post_id, posts.created_at post_created_at, users.username username, users.id user_id')
                                  .where(name: 'accepted_answer_post_id')
+                                 .order("topic_custom_fields.created_at DESC")
                                  .limit(5)
-                                 .order("DATE(topic_custom_fields.created_at)")
 
         custom_field.each_with_index do |field, index|
           user = User.find_by_id(field.user_id)
-          solved_answers[index] = {username: field.username, user_avatar: user.avatar_template_url.gsub("{size}", "25"), topic_title: field.topic.title, topic_url: field.topic.url, category_name: field.topic.category.name, post_created_at: field.post_created_at}
+          solved_answers[index] = {username: field.username, user_avatar: user.avatar_template_url.gsub("{size}", "25"), topic_title: field.topic.title, topic_url: field.topic.url, category_name: field.topic.category.name, post_created_at: field.post_created_at, solution_marked_at: field.created_at}
         end
         solved_answers
-      }
+      end
     end
   end
 end
